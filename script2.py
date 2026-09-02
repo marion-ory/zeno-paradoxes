4dcd1d365bff5d5e2f039e876eb622b88ceea8f1
Marion Ory <marionory@laplateforme.io>
2026-09-02 08:44:47 +0200

    algorithme dichotomie

---
+++ script2.py
@@ -0,0 +1,20 @@
+compteur = 0
+position_Arbre = 8.0
+position_Pierre = 0.0
+
+seuil = 0.001
+
+distance = 8.0
+ecart = position_Arbre - position_Pierre
+
+
+while ecart > seuil:
+
+    pas = ecart / 2
+    position_Pierre += pas
+    ecart = position_Arbre - position_Pierre
+    compteur += 1
+
+    print(f"La cible, l'arbre est à {position_Arbre} mètres")
+    print(f"l'écart entre la pierre et l'arbre : {ecart} mètres ")
+    print(f"\nLa pierre est à {pas} metre de l'arbre")
